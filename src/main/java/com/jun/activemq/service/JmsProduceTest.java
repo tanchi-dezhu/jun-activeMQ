12619fc9f060bcd718ba34b05caf260daa8bb018
jun <884720982@qq.com>
2021-05-16 11:17:04 +0800

    手敲activemq队列

---
+++ src/main/java/com/jun/activemq/service/JmsProduceTest.java
@@ -0,0 +1,33 @@
+package com.jun.activemq.service;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @author jun
+ * @date 2021年05月16日 11:05
+ */
+public class JmsProduceTest {
+
+    private static final String ACTIVEMQ_URL = "tcp://114.116.251.9:61616";
+    private static final String QUEUE_NAME = "queue01";
+
+    public static void main(String[] args) throws JMSException {
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue(QUEUE_NAME);
+        MessageProducer messageProducer = session.createProducer(queue);
+        for (int i = 0; i < 3; i++) {
+            TextMessage textMessage = session.createTextMessage(String.valueOf(i));
+            messageProducer.send(textMessage);
+        }
+
+        messageProducer.close();
+        session.close();
+        System.out.println("****消息发布到MQ队列完成");
+        System.exit(-1);
+    }
+}
