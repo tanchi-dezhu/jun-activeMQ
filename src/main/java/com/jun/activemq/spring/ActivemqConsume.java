f8e179c5b7ce0d698eb2e1f745c318844e9ff687
jun <884720982@qq.com>
2021-05-25 20:03:00 +0800

    spring和aticemq整合并且配置applicationContext.xml

---
+++ src/main/java/com/jun/activemq/spring/ActivemqConsume.java
@@ -0,0 +1,8 @@
+package com.jun.activemq.spring;
+
+/**
+ * @author jun
+ * @date 2021年05月25日 19:46
+ */
+public class ActivemqConsume {
+}
---
+++ src/main/java/com/jun/activemq/spring/ActivemqProduce.java
@@ -0,0 +1,29 @@
+package com.jun.activemq.spring;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.jms.TextMessage;
+
+/**
+ * @author jun
+ * @date 2021年05月25日 19:46
+ */
+@Service
+public class ActivemqProduce {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ActivemqProduce activemqProduce = (ActivemqProduce) ctx.getBean("activemqProduce");
+        activemqProduce.jmsTemplate.send(session -> {
+            TextMessage textMessage = session.createTextMessage("***spring和ActiveMq整合的case");
+            return textMessage;
+        });
+    }
+}
